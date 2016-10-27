27e76a66ea0e12faa41156f9f6f8a5c0fb0b0aa5
luowenx <1056084639@qq.com>
2016-10-27 12:35:00 +0800

    Initial Commit

---
+++ FadeInAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FadeInAnimation
+//
+//  Created by luowenx on 16/10/27.
+//  Copyright © 2016年 xtkj_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FadeInAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FadeInAnimation
+//
+//  Created by luowenx on 16/10/27.
+//  Copyright © 2016年 xtkj_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
